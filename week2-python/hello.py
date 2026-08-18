39593a5012baa38b589541bd921234102e27b209
oezdayi <oezdayi@gmail.com>
2026-08-18 17:21:49 +0200

    week 2: first python script with input and f-strings

---
+++ week2-python/hello.py
@@ -0,0 +1,11 @@
+
+name = input("Wie heißt du? ")
+alter = int(input("Wie alt bist du? "))
+lernt_python = True
+
+print("Name:", name)
+print(f"Alter: {alter}")
+print(f"{name} ist {alter} Jahre alt")
+print("Lernt Python:", lernt_python) 
+
+print (f"In 10 Jahren bist du {alter + 10} Jahre alt")
